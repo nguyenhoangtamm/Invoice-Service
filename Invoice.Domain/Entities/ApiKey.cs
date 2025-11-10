800bb4afdbea16fc73501cc2cb8f369d6c7c695a
Nguyễn Thanh Phương <phuongg6324@gmail.com>
2025-11-10 20:33:32 +0700

    add Entities

---
+++ Invoice.Domain/Entities/ApiKey.cs
@@ -0,0 +1,14 @@
+﻿using Invoice.Domain.Entities.Base;
+
+namespace Invoice.Domain.Entities;
+
+public class ApiKey : BaseAuditableEntity
+{
+    public string KeyHash { get; set; } = default!;
+    public string? Name { get; set; }
+    public bool Active { get; set; } = true;
+    public DateTime? RevokedAt { get; set; }
+
+    public int OrganizationId { get; set; }
+    public Organization Organization { get; set; } = default!;
+}
---
+++ Invoice.Domain/Entities/Organization.cs
@@ -0,0 +1,21 @@
+﻿using Invoice.Domain.Entities.Base;
+
+namespace Invoice.Domain.Entities;
+
+public class Organization : BaseAuditableEntity
+{
+    public string OrganizationName { get; set; } = default!;
+    public string? OrganizationTaxId { get; set; }
+    public string? OrganizationAddress { get; set; }
+    public string? OrganizationPhone { get; set; }
+    public string? OrganizationEmail { get; set; }
+    public string? OrganizationBankAccount { get; set; }
+
+    // Owner (User)
+    public int UserId { get; set; }
+    public User User { get; set; } = default!;
+
+    // Navigation
+    public ICollection<ApiKey> ApiKeys { get; set; } = new List<ApiKey>();
+    public ICollection<Invoice> Invoices { get; set; } = new List<Invoice>();
+}
