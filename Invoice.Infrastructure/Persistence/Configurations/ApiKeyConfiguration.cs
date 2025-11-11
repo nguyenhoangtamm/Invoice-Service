a924caf4615d4ea9a67572f77283862f26d5a417
Nguyễn Thanh Phương <phuongg6324@gmail.com>
2025-11-11 13:37:34 +0700

    create ef configuration

---
+++ Invoice.Infrastructure/Persistence/Configurations/ApiKeyConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Invoice.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Invoice.Infrastructure.Persistence.Configurations;
+
+public class ApiKeyConfiguration : IEntityTypeConfiguration<ApiKey>
+{
+    public void Configure(EntityTypeBuilder<ApiKey> b)
+    {
+        b.ToTable("Api_Keys");
+
+        b.HasKey(x => x.Id);
+
+        b.Property(x => x.KeyHash)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        b.Property(x => x.Name)
+            .HasMaxLength(200);
+
+        b.HasOne(x => x.Organization)
+            .WithMany(o => o.ApiKeys)
+            .HasForeignKey(x => x.OrganizationId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Invoice.Infrastructure/Persistence/Configurations/OrganizationConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Invoice.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Invoice.Infrastructure.Persistence.Configurations;
+
+public class OrganizationConfiguration : IEntityTypeConfiguration<Organization>
+{
+    public void Configure(EntityTypeBuilder<Organization> b)
+    {
+        b.ToTable("Organizations");
+
+        b.HasKey(x => x.Id);
+
+        b.Property(x => x.OrganizationName)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        b.Property(x => x.OrganizationTaxId)
+            .HasMaxLength(50);
+
+        b.Property(x => x.OrganizationAddress)
+            .HasMaxLength(500);
+
+        b.Property(x => x.OrganizationPhone)
+            .HasMaxLength(30);
+
+        b.Property(x => x.OrganizationEmail)
+            .HasMaxLength(200);
+
+        b.Property(x => x.OrganizationBankAccount)
+            .HasMaxLength(100);
+
+        // Owner
+        b.HasOne(x => x.User)
+            .WithMany(u => u.Organizations)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
+
